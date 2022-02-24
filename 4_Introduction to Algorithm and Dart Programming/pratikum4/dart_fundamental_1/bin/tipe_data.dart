220f68670c9ee98224b55bcd91e86f8531b185ae
MacharaniRaschintasofi <macharaniras.smkn4jbi@gmail.com>
2022-02-24 12:41:31 +0700

    add files

---
+++ 4_Introduction to Algorithm and Dart Programming/pratikum4/dart_fundamental_1/bin/tipe_data.dart
@@ -0,0 +1,10 @@
+import 'dart:ffi';
+void main (){
+  int usia = 20;
+  double tinggiBadan = 158.09;
+  print(tinggiBadan);
+  bool wanita = true;
+  print(wanita);
+  String nama = 'Sofi';
+  print(nama);
+}
