ce9732acb2d9be41ea1fe05b96786762428274e1
MacharaniRaschintasofi <macharaniras.smkn4jbi@gmail.com>
2022-02-28 12:50:25 +0700

    Add dart_fundamental_3 to pratikum5

---
+++ 5_Basic Dart Programming/pratikum5/dart_fundamental_3/bin/async_await.dart
@@ -0,0 +1,29 @@
+// void p1(){
+//   Future.delayed(Duration(seconds: 1),(){
+//     print('Hello by p1');
+//   });
+// }
+
+// void p2(){
+//   print('Hello by p2');
+// }
+
+// void main(){
+//   p1();
+//   p2();
+// }
+
+Future<void> p1()async{
+  await Future.delayed(Duration(seconds: 1),(){
+    print('Hello by p1');
+  });
+}
+
+void p2(){
+  print('Hello by p2');
+}
+
+void main()async{
+  await p1();
+  p2();
+}
