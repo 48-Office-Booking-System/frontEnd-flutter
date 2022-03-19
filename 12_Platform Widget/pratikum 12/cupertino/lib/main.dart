8bfd8ad1b743e164855cd6415f18ef97831931c2
MacharaniRaschintasofi <macharaniras.smkn4jbi@gmail.com>
2022-03-19 17:23:51 +0700

    push sc

---
+++ 12_Platform Widget/pratikum 12/cupertino/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Chats ';
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        // Try removing opacity to observe the lack of a blur effect and of sliding content.
+        backgroundColor: Color.fromARGB(255, 255, 255, 255).withOpacity(0.5),
+        middle: const Text('Chats'),
+      ),
+      child: Column(
+        children: <Widget>[
+          Container(height: 50, color: Color.fromARGB(255, 255, 255, 255)),
+        ],
+      ),
+    );
+  }
+}
--- 12_Platform Widget/summary.md
+++ 12_Platform Widget/summary.md
@@ -1,6 +1,22 @@
 Nama : Macharani Raschintasofi
 Kelas : D
 
+Memanfaatkan gaya yang berbeda pada Android & iOS
+:
+-MaterialApp :
+*Widget dasar yang mengemas seluruh widget dalam aplikasi
+*Widget yang digunakan pada sistem Android
+*Di-import dari package:flutter/material.dart
+*Widget yang pertama kali dibuka diletakkan pada bagian home
+\*Mengatur halaman juga dapat dilakukan dengan menggunakan routes dan initialRoute
+
+-CupertinoApp :
+*Widget dasar yang mengemas seluruh widget dalam aplikasi
+*Widget yang digunakan pada sistem iOS
+\*Di-import dari package:flutter/cuppertino.dart
+
+Scaffold : Widget dasar untuk membangun sebuah halaman pada MaterialApp. Membentu tata letak dasar sebuah halaman yang ditulis melalui properti-properti .
+
 Flutter = tools atau alat pengembangan antar muka pengguna dari google yang digunakan untuk membuat aplikasi mobile , desktop, dan web .
 
 Keunggulan :
