573dc0302e359ff38c34ead5a9979c5f10f618ba
Macharani Raschintasofi <macharaniras.smkn4jbi@gmail.com>
2022-05-09 15:02:54 +0700

    push

---
+++ Mini Project/Program/quba_app/lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,49 @@
+import 'package:quba_app/app/data/models/surah.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class Home extends GetView<HomeController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Beranda'),
+        centerTitle: true,
+      ),
+      body: FutureBuilder<List<Surah>>(
+        future: controller.getAllSurah(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (!snapshot.hasData) {
+            return Center(
+              child: Text("Tidak ada data."),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              Surah surah = snapshot.data![index];
+              return ListTile(
+                onTap: () {},
+                leading: CircleAvatar(
+                  child: Text("${surah.number}"),
+                ),
+                title: Text("${surah.name?.transliteration?.id ?? 'Error..'}"),
+                subtitle: Text(
+                    "${surah.numberOfVerses} Ayat | ${surah.revelation?.id ?? 'Error..'}"),
+                trailing: Text("${surah.name?.short ?? 'Error..'}"),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ Mini Project/Program/quba_app/lib/app/modules/introduction/views/introduction_view.dart
@@ -0,0 +1,54 @@
+import 'package:quba_app/app/routes/app_pages.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:get/get.dart';
+
+import '../controllers/introduction_controller.dart';
+
+class IntroductionView extends GetView<IntroductionController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "QUBA ",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 50,
+              ),
+              child: Text(
+                "Alquran diturunkan untuk menjadi obat bagi penyakit manusia dan rahmat bagi orang-orang yang beriman(QS.Al-Isra:82)",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: 300,
+              height: 300,
+              child: Lottie.asset(
+                "assets/lotties/animasi1.json",
+              ),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () => Get.offAllNamed(Routes.HOME),
+              child: Text("Mulai dengan Bismillah"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
