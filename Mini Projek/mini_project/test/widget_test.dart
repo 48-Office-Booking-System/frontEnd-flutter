a52e375695dd9c120731fa1a91fc1dc4529570ad
Macharani Raschintasofi <macharaniras.smkn4jbi@gmail.com>
2022-05-20 10:52:35 +0700

    push

--- Mini Projek/mini_project/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package :https/http.dart' as http;
-
-void main()async{
-  Url url = Url.("https://api.quran.sutanlab.id/surah");
-  var res =await http.get(url);
-
-  print (res.body);
-}
--- Mini Projek/splashscreen/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package :https/http.dart' as http;
-
-void main()async{
-  Uri url = Uri.("https://api.quran.sutanlab.id/surah");
-  var res =await http.get(url);
-
-  print (res.body);
-}
